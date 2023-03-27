06bca574d52832d7d88fbbf0c29d62e208936fda
Personal <Personal@DESKTOP-856HCH1>
2023-03-28 00:55:28 +0500

    First commit message

---
+++ TASK_1_Soft_delay.c
@@ -0,0 +1,35 @@
+/*Maaz khan
+ * Reg No : 19jzele0320
+ */
+#include "F28x_Project.h"
+#define LED1 GpioDataRegs.GPBDAT.bit.GPIO34
+#define LED2 GpioDataRegs.GPADAT.bit.GPIO31
+
+
+void main(){
+    //Initialization of System Control
+    InitSysCtrl();
+    //Initialization of General Purpose Input Output Pins
+    InitGpio();
+    EALLOW;
+    //Configure GPIO34 and  GPIO31 as an Output
+    GpioCtrlRegs.GPAMUX2.bit.GPIO31 = 0;
+    GpioCtrlRegs.GPADIR.bit.GPIO31 = 1;
+    GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+    GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
+    GpioDataRegs.GPASET.bit.GPIO31 = 0; //Initially BLUE LED will turn ON
+    GpioDataRegs.GPBSET.bit.GPIO34 = 0; //Initially RED LED will turn OFF
+    EDIS;
+    while(1)
+       {
+           LED_1=1;
+           DELAY_US(2000000);
+           LED_1=0;
+           DELAY_US(2000000);
+           LED_1=0;
+           DELAY_US(2000000);
+           LED_2=0;
+           DELAY_US(2000000);
+
+       }
+   }
